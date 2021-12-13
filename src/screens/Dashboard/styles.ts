1c40fc6e232d975780a25b51e0b2a74a3dd1db49
Ian Peres Coelho <ian.coel@hotmail.com>
2021-12-13 16:00:32 -0300

    global styles

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -4,11 +4,11 @@ export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  background-color: violet;
+  background-color: ${({ theme }) => theme.colors.background};
 `;
 
 export const Title = styled.Text`
   font-size: 24px;
   font-weight: bold;
-  color: black;
+  color: ${({ theme }) => theme.colors.title};
 `;
