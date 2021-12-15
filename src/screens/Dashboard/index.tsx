75675e96903be9363b2b801d10dd0d5af9b2609b
Ian Peres Coelho <ian.coel@hotmail.com>
2021-12-15 15:37:56 -0300

    hilightcards container

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -10,6 +10,7 @@ import {
   UserGreeting,
   UserName,
   Icon,
+  HighlightCards,
 } from './styles';
 
 const Dashboard = () => {
@@ -32,7 +33,11 @@ const Dashboard = () => {
         </UserWrapper>
       </Header>
 
-      <HighlightCard />
+      <HighlightCards>
+        <HighlightCard />
+        <HighlightCard />
+        <HighlightCard />
+      </HighlightCards>
     </Container>
   );
 };
